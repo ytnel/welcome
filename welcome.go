314a5336bc5b2ec808add47c228c9eb0c2bb89ea
ytnel <ytkim.korea@gmail.com>
2020-09-01 11:34:08 +0900

    new file

---
+++ welcome.go
@@ -0,0 +1,14 @@
+package welcome
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome from the language of the coming decade - Go!")
+}
